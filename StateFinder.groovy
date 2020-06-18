e5080a38276c3ac498d4fa5713c643cb36e958a2
Mika Nokka <mika.nokka@ambientia.fi>
2020-06-18 14:00:46 +0300

    Can calculate using first-time-to-state custom fields how lon it took to stay in state1 before going state2

--- StateFinder.groovy
+++ StateFinder.groovy
@@ -1,3 +1,7 @@
+// mika.nokka1@gmail.com , June 2020, Hämeenlinna, Finland
+// POC: Uses SCriptRunner "first time to state" custom field pair to calculate "how long was spent in first state"
+
+
 import com.atlassian.jira.ComponentManager
 import com.atlassian.jira.issue.Issue
 import com.atlassian.jira.issue.comments.Comment
@@ -22,7 +26,7 @@ log.setLevel(Level.DEBUG)
 def open="First time in Open"
 def closed ="First Closed time"
 
-log.debug("---------- StateFinder started ---------------------------------------")
+//log.debug("---------- StateFinder started ---------------------------------------")
 
 //def changeItems = ComponentAccessor.getChangeHistoryManager().getAllChangeItems(issue)
 
@@ -32,47 +36,57 @@ def OpenDate = issue.getCustomFieldValue(field)
 def field2 = customFieldManager.getCustomFieldObjectByName(closed)
 def CloseDate = issue.getCustomFieldValue(field2)
 
-def Text="NotClosedYet"
+def totalhours="NotAvailableYet"
 if (OpenDate == null || CloseDate == null) {
-	Text="NotClosedYet" 
+	def Text="NotClosedYet" 
     }
 else {
-  	Text=CloseDate
-  	//result=TimeCategory.minus(CloseDate, OpenDate).days
-    //def duration = OpenDate - CloseDate
-    //log.debug( "result: ${result}")
+  	//Text=CloseDate
     
-    def pattern = "dd.MM.yyyy hh:mm"
-	def input = "17.06.2020 12:23"
-	def input2 = "19.06.2020 15:00"
-
-	def date = new SimpleDateFormat(pattern).parse(input)
-	def date2 = new SimpleDateFormat(pattern).parse(input2)
-    def minus=date2-date
-    log.debug( "First date: ${date}, SECOND date :${date2}")  
-    log.debug( "minus date: ${minus}") 
+    def CloseDateStr = CloseDate.toString()
+    def OpenDateStr = OpenDate.toString()
     
-    def duration = TimeCategory.minus(date2, date)
-    log.debug( "duration date: ${duration}")
+    //def pattern = "dd.MM.yyyy hh:mm"
+    // this was example to check function usages
+	//def input = "17.06.2020 12:23"
+	//def input2 = "19.06.2020 15:00"
+	//def date = new SimpleDateFormat(pattern).parse(input)
+	//def date2 = new SimpleDateFormat(pattern).parse(input2)
+    //def minus=date2-date
+    //log.debug( "First date: ${date}, SECOND date :${date2}")  
+    //log.debug( "minus date: ${minus}")  
+    //def duration = TimeCategory.minus(date2, date)
+    //log.debug( "duration date: ${duration}")
+    //def hours=duration.getHours()
+    //def days=duration.getDays()
+    //log.debug( "duration hours: ${hours}")
+    
+    def pattern = "yyyy-MM-dd hh:mm:ss.SSS"
+    def start = new SimpleDateFormat(pattern).parse(OpenDateStr)
+	def end = new SimpleDateFormat(pattern).parse(CloseDateStr)
+    def duration = TimeCategory.minus(end, start)
     def hours=duration.getHours()
     def days=duration.getDays()
-    log.debug( "duration hours: ${hours}")
     
-    def totalhours= days*24 + hours
-    log.debug( "duration totalhours: ${totalhours}")
-    
-   //def String pattern = "dd-MM-yyyy";
-   //SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-   //def String simpletime = simpleDateFormat.format(input);
-   //   log.debug( "TOKA date: ${simpletime}") 
+    if (days == null) {
+                    days=0
+                }
+    if (hours== null) {         
+                    hours=0
+                }
+            
+
+    totalhours= days*24 + hours
+    //log.debug( "duration totalhours: ${totalhours}")
+    if (totalhours <0) {
+       totalhours="MoreThanOneCycleDone"   // issue had been reopened, cant calcualte as first time to state ScriptRunner fields used                         
+                 }
+
+
     
 }
     
-  
-    
+log.debug ("${issue}: Opened:${OpenDate}, Released(closed):${CloseDate}, Hours spent(open):${totalhours}")
 
-log.debug ("open: ${OpenDate}")
-log.debug ("closed: ${CloseDate}")
-
-
-return Text
+//log.debug("---------- StateFinder exiting ---------------------------------------")
+return totalhours
